bec7e1ae0d436dfd04194f1120e5d98dda3f9284
tabithaondego <nyancheratabitha8@gmail.com>
2026-05-14 10:25:13 -0700

    First commit

---
+++ units.c
@@ -0,0 +1,22 @@
+//Program to display all the units offered this semester
+
+/*
+Description : Program to display units offered this semester
+Name : Tabitha Ondego
+Date : 14/05/2026
+REG NO: BCS-03-0073/2026
+Version 1 
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("Introduction to Physics \n");
+	printf("Basic Mathematics \n");
+	printf("Computation Logic \n");
+	printf("Communication skills \n");
+	printf("Structured Programming \n");
+	
+	return 0;
+}
